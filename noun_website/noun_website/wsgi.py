800d226672caa0731957a7c2f835e7fca8a66051
Aryan Shiv <75351276+AryanMess@users.noreply.github.com>
2025-04-21 20:59:48 +0530

    Update wsgi.py

--- noun_website/noun_website/wsgi.py
+++ noun_website/noun_website/wsgi.py
@@ -14,6 +14,6 @@ from django.core.wsgi import get_wsgi_application
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "noun_website.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "noun_website.noun_website.settings")
 
 application = get_wsgi_application()
