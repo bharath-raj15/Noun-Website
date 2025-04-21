c455e4efeba27347c334ea20cfcc2c86ae4d46cf
Aryan Shiv <75351276+AryanMess@users.noreply.github.com>
2025-04-21 21:11:20 +0530

    Update apps.py

--- noun_website/noun_homepage/apps.py
+++ noun_website/noun_homepage/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class NounHomepageConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "noun_homepage"
+    name = "noun_website.noun_homepage"
